03a4d568e80a39bed71ae44633325af044f9efbb
Claudio Javier Mansilla Brito <cmansilla2016@udec.cl>
2023-09-11 19:30:47 -0300

    Fin clase de objetos, como hacer un freze

--- assets/js/objeto-literal.js
+++ assets/js/objeto-literal.js
@@ -30,3 +30,39 @@ console.log('Vivo', personaje[x]);
 
 console.log('Ultima pelicula', personaje['ultimaPelicula']);
 
+// Mas detalles
+
+delete personaje.edad // Elimina la propiedad edad del objeto personaje
+console.log(personaje)
+
+personaje.casado = true;// Podemos agregarle atributos a un objeto
+
+const entriesPares = Object.entries(personaje);// Devuelve un arreglo con las propiedades del objeto
+console.log(entriesPares);
+
+// Los comandos delete o que modifique elementos de un objeto y tambien el agregar
+// nuevos atributos al objeto puede ser evitado. Se podria pensar que se puede hacer
+// definiendo el objeto con const en vez de let, pero eso no sirve.
+// Para evitar estos cambios hay que usar el comando freeze.
+
+Object.freeze(personaje);
+
+personaje.casado = false; // Este comando no funcionara, ya que intentamos cambiar el atributo despues de un freeze
+
+// Por otro lado, los objetos dentro del objeto no se protegen, es decir
+// el objeto direccion puede ser cambiado
+
+personaje.direccion.ubicacion = 'Chile' // Esto cambia el atributo Malibu, California por Chile
+console.log(personaje);
+
+Object.freeze(personaje.direccion);// Esto nos permite dejar permanente el objeto direccion dentro del objeto personaje
+
+personaje.direccion.ubicacion = 'Argentina'
+console.log(personaje);
+
+const propiedades = Object.getOwnPropertyNames(personaje);// Devuelve un arreglo con las propiedades del objeto
+const valores = Object.values(personaje);// Devuelve un arreglo con los valores del objeto
+console.log(propiedades,valores);
+
+
+
