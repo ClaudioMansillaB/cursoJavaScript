73c36430b0a15eda2dbeeceb321c38539d8e0939
Claudio Javier Mansilla Brito <cmansilla2016@udec.cl>
2023-09-11 14:34:39 -0300

    Comentarios agregados en arreglos2.js

--- assets/js/arreglos2.js
+++ assets/js/arreglos2.js
@@ -7,25 +7,25 @@ let ultimo = juegos[juegos.length - 1];
 
 console.log({ primero, ultimo });
 
-juegos.forEach( (elemento, indice, arr) => {
+juegos.forEach( (elemento, indice, arr) => { // Recorre el arreglo
     console.log({ elemento, indice, arr });
 }); 
 
-let nuevaLongitud = juegos.push('F-Zero');
+let nuevaLongitud = juegos.push('F-Zero'); // Agrega un elemento al final del arreglo
 console.log({ nuevaLongitud, juegos});
 
-nuevaLongitud = juegos.unshift('Fire emblem');
+nuevaLongitud = juegos.unshift('Fire emblem'); // Agrega un elemento al inicio del arreglo
 console.log({nuevaLongitud, juegos});
 
-let juegoBorrado = juegos.pop();
+let juegoBorrado = juegos.pop(); // Borra el ultimo elemento del arreglo
 console.log({ juegoBorrado, juegos });
 
 let pos = 1;
 console.log(juegos);
 
-let juegosBorrados = juegos.splice(pos, 2);
+let juegosBorrados = juegos.splice(pos, 2); // Se borran 2 elementos a partir de la posicion pos
 
 console.log({ juegosBorrados, juegos });
 
-let metroidIndex = juegos.indexOf('Metroid');
+let metroidIndex = juegos.indexOf('Metroid'); // Devuelve el indice del elemento buscado
 console.log({ metroidIndex });
