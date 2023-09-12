201a3c4d2aca8d09fbf360f135d35c3e9558e573
Claudio Javier Mansilla Brito <cmansilla2016@udec.cl>
2023-09-12 12:59:12 -0300

    Mas funciones, return y modulo random

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -17,9 +17,16 @@ const saludar2 = function(){
 // Podemos hacer funciones mas dinamicas pidiendo valores.
 
 function saludar3(nombre){
-    console.log(arguments);// Arguments es una funcion nativa de las funciones que se llaman con function,
+    //console.log(arguments);// Arguments es una funcion nativa de las funciones que se llaman con function,
                            // Las funciones flecha no las tienen.
-    console.log('Hola ' + nombre);
+    //console.log('Hola ' + nombre);
+    
+    // Si queremos retornar mas de un valor, se debe hacer en una lista y luego
+    // pedirlo a traves de los indices de la lista.
+    return [1,2,3,4,5];
+
+    console.log('Codigo despues del return'); // Esto nunca se ejecuta ya que esta despues de un return.
+
 }
 
 // Funciones de flecha o funciones lambda
@@ -32,11 +39,38 @@ const saludarFlecha2 = ( nombre ) =>{
     console.log('Hola', nombre);
 }
 
-saludarFlecha();
-saludarFlecha2('Javiera');
+const retornoSaludar = saludar3('Claudio', 40, true, 'Chile');
+//console.log(retornoSaludar[0],retornoSaludar[1],retornoSaludar[2]);
+
+// saludarFlecha();
+// saludarFlecha2('Javiera');
 
 
+function sumar( a,b ){
+    return a + b;
+}
 
+// console.log( sumar(1,2));
 
-saludar3('Claudio', 40, true, 'Chile');
-// saludar2();
+const sumar2 = (a,b) => {
+    return a + b;
+}
+
+// console.log(sumar2(3,2));
+
+// Si solamente necesitamos hacer un return y nada mas dentro de una funcion de flecha
+// Podemos resumirla de la siguiente formma
+
+const sumar3 = (a,b) => a + b;
+
+console.log(sumar3(7,2));
+
+function getAleatorio(){
+    return Math.random();
+}
+
+//console.log(getAleatorio());
+
+const getAleatorio2 = () => Math.random();
+
+console.log('Aleatorio2',getAleatorio2());
