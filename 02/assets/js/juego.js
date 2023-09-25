5a8dd08722c92e64a7c57017d5a5377020504fbb
Claudio Javier Mansilla Brito <cmansilla2016@udec.cl>
2023-09-25 16:52:35 -0300

    Deck creado en js

---
+++ 02/assets/js/juego.js
@@ -0,0 +1,34 @@
+/* 
+2C = two of clobes (trebol)
+2D = two of diamonds (trebol)
+2H = two of hearts (trebol)
+2S = two of spades (trebol)
+
+*/
+
+let deck         = [];
+const tipos      = ['C','D','H','D'];
+const especiales = ['A','J','Q','K'];
+
+const crearDeck  = () => {
+ 
+    for(let i = 2; i <= 10; i++) {
+        for(let tipo of tipos) {
+            deck.push(i + tipo);
+        }   
+        
+    }
+    for(let tipo of tipos){
+        for(let especial of especiales){
+            deck.push(especial + tipo);
+            
+        }
+    }
+    
+    console.log(deck);
+
+}
+
+
+
+crearDeck();
