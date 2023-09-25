44d32ede81e5b422c70d02d6565ddf10e3be8897
Claudio Javier Mansilla Brito <cmansilla2016@udec.cl>
2023-09-25 17:24:02 -0300

    Peticion de carta hecho

--- 02/assets/js/juego.js
+++ 02/assets/js/juego.js
@@ -10,6 +10,7 @@ let deck         = [];
 const tipos      = ['C','D','H','D'];
 const especiales = ['A','J','Q','K'];
 
+// Esta funcion crea un nuevo Deck
 const crearDeck  = () => {
  
     for(let i = 2; i <= 10; i++) {
@@ -23,12 +24,28 @@ const crearDeck  = () => {
             deck.push(especial + tipo);
         }
     }
-    console.log(deck);
+    //console.log(deck);
     deck = _.shuffle(deck);
     console.log(deck);
-    
+    return deck;
 }
 
-
-
 crearDeck();
+
+
+// Esta funcion me permite tomar una carta
+
+const pedirCarta = () => {
+
+    if (deck.length === 0) {
+        throw 'No hay cartas en el deck';
+    }
+
+    const carta = deck.shift();
+
+    console.log(deck);
+    console.log(carta);
+    return carta;
+}
+deck = [];
+pedirCarta();
